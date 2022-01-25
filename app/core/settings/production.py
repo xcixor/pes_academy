eb555511dd10ba756b0f72597644a918dab07bcb
xcixor <pndungu54@gmail.com>
2022-01-25 10:03:01 +0300

    chore(settings): add media settings in production

--- app/core/settings/production.py
+++ app/core/settings/production.py
@@ -2,3 +2,8 @@ from .base import *
 DEBUG = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+# media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
