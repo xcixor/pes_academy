c26033ffcb6641fb56ae12b2141a4086d6300f4f
xcixor <pndungu54@gmail.com>
2022-02-02 17:52:37 +0300

    chore(db): add a many to many fields in milestone model to enable the creation of several businesses with the same milestone

--- app/accounts/models/milestone.py
+++ app/accounts/models/milestone.py
@@ -4,9 +4,8 @@ from accounts.models import BusinessOrganization
 
 class Milestone(models.Model):
     milestone = models.CharField(max_length=255)
+    businesses = models.ManyToManyField(
+        BusinessOrganization, blank=True, related_name='businesses')
 
     def __str__(self) -> str:
         return self.milestone
-
-
-# businessesmilestonesmodel
