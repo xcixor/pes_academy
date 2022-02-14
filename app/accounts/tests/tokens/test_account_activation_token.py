7e037e9ea1ad55758ce143cd804960fb14b5577c
xcixor <pndungu54@gmail.com>
2022-02-14 15:44:07 +0300

    chore(tokens): create a user activation token

---
+++ app/accounts/tests/tokens/test_account_activation_token.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from accounts.tokens import account_activation_token
+from accounts.tests.common import AccountsBaseTestCase
+from django.utils.http import urlsafe_base64_decode
+
+
+class AccountActivationTokenTestCase(AccountsBaseTestCase):
+
+    def setUp(self) -> None:
+        super(AccountActivationTokenTestCase, self).setUp()
+
+    def test_generates_valid_token(self):
+        user = get_user_model().objects._create_user(
+            'test@gmail.com', 'password')
+        token = account_activation_token.make_token(user)
+        self.assertTrue(token)
+        self.assertTrue(account_activation_token.check_token(user, token))
