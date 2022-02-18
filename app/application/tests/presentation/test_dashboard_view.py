50322494741cd5443bd8d83ea51b2f3383691c7c
Margaret mwangi <maggiemwas91@gmail.com>
2022-02-18 16:25:45 +0300

    chore(templates): Edited dashboard.html

--- app/application/tests/presentation/test_dashboard_view.py
+++ app/application/tests/presentation/test_dashboard_view.py
@@ -3,14 +3,14 @@ from application.presentation.views  import DashboardView
 from django.test import TestCase
 
 
-class SubmitViewTestCase(TestCase):
+class DashboardViewTestCase(TestCase):
 
     def setUp(self):
-        super(SubmitViewTestCase, self).setUp()
+        super(DashboardViewTestCase, self).setUp()
 
     def test_view_properties(self):
-        self.assertEqual(SubmitView.template_name, 'dashboard.html')
-        self.assertTrue(issubclass(SubmitView, TemplateView))
+        self.assertEqual(DashboardView.template_name, 'dashboard.html')
+        self.assertTrue(issubclass(DashboardView, TemplateView))
 
     def test_successfully_gets_submit_page(self):
         response = self.client.get('/applications/dashboard/')
