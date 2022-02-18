e7cb5bd7b086f6a0d6b3585b00042ff5826b7a55
Margaret mwangi <maggiemwas91@gmail.com>
2022-02-18 11:17:49 +0300

    chore(templates):Add tabs to the dashboard to link to pes academy and the ongoing applications

--- app/common/static/js/common/base.js
+++ app/common/static/js/common/base.js
@@ -11,3 +11,20 @@ $(document).ready(function(){
         $("#navigation").toggleClass("hidden-xs");
     });
  });
+
+ function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "none";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
