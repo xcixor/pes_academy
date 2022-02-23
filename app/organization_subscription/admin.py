4689ca94e31e754326bd99e4f63d4493570a3585
xcixor <pndungu54@gmail.com>
2022-02-23 14:39:29 +0300

    chore(admin): register organization subscriptions model

--- app/organization_subscription/admin.py
+++ app/organization_subscription/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from organization_subscription.models import OrganizationSubscription
+from accounts.models import User
 
-# Register your models here.
+
+class SubscribersInline(admin.TabularInline):
+
+    model = User
+    extra = 0
+
+
+# @admin.register(OrganizationSubscription)
+# class OrganizationSubscriptionAdmin(admin.ModelAdmin):
+
+#     inlines = [SubscribersInline]
+
+admin.site.register(OrganizationSubscription)
+
