9757133181e03e9c0153c3b3c87aa91f836f0a69
xcixor <pndungu54@gmail.com>
2022-02-24 10:53:30 +0300

    refactor(subscriptions): remove unnecessary code

--- app/organization_subscription/presentation/views/activate_subscription.py
+++ app/organization_subscription/presentation/views/activate_subscription.py
@@ -1,27 +1,14 @@
 from django.views import View
-from django.shortcuts import render, redirect
-from django.contrib.auth import get_user_model
+from django.shortcuts import redirect
 from django.contrib import messages
-from accounts.tokens import account_activation_token
 
 
 class ActivateSubscriptionView(View):
 
     def get(self, request, *args, **kwargs):
-        uidb64 = kwargs.pop('uidb64')
-        token = kwargs.pop('token')
-        user_model = get_user_model()
-        user = user_model.objects.get_user_by_uid(uidb64)
-        if user is not None and account_activation_token.check_token(user, token):
-            user.is_active = True
-            user.save()
-            success_message = (
-                f'Congratulations {user}, your have joined your '
-                'organization\'s workspace. Please register to continue!')
-            messages.add_message(
-                request, messages.SUCCESS, success_message)
-            return redirect('/accounts/register/')
-        error_message = ('Uh oh! something went wrong.')
+        success_message = (
+            'Congratulations, your have joined your '
+            'organization\'s workspace. Please register to continue!')
         messages.add_message(
-            request, messages.SUCCESS, error_message)
-        return render(request, 'registration/account_activation_invalid.html')
+            request, messages.SUCCESS, success_message)
+        return redirect('/accounts/register/')
