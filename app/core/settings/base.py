6cb6f8c3acf57f154d9ac99d9578e2e0a08c0349
xcixor <pndungu54@gmail.com>
2022-03-03 10:55:12 +0300

    fix(ci): configure redis in docker to fix failing redis tests

--- app/core/settings/base.py
+++ app/core/settings/base.py
@@ -179,13 +179,12 @@ VERIFIED_EMAIL_USER = os.getenv('VERIFIED_EMAIL_USER')
 # redis
 
 REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1')
-REDIS_PORT = os.environ.get('REDIS_PORT', '6379')
 REDIS_DB = os.environ.get('REDIS_DB', '1')
 
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': f'redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}',
+        'LOCATION': f'redis://{REDIS_HOST}:6379/{REDIS_DB}',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
