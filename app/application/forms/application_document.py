a1c331ee703e388d2309a15c8689641c4ccf516e
xcixor <pndungu54@gmail.com>
2022-03-10 16:59:52 +0300

    chore(forms): update document if already created

--- app/application/forms/application_document.py
+++ app/application/forms/application_document.py
@@ -7,3 +7,13 @@ class ApplicationDocumentForm(forms.ModelForm):
     class Meta:
         model = ApplicationDocument
         fields = '__all__'
+
+    def save(self, commit=True):
+        document, created = ApplicationDocument.objects.update_or_create(
+            document_name=self.cleaned_data['document_name'],
+            defaults={
+                'document': self.cleaned_data['document'],
+                'application': self.cleaned_data['application']
+            }
+        )
+        return document
