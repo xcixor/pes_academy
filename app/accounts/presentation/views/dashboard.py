a33e7c5aa0d038978d13e867e614dd592871fd2d
margaret254 <maggiemwas91@gmail.com>
2022-03-11 11:16:07 +0300

    chore(templates): create a form for the users to upload extra documents

--- app/accounts/presentation/views/dashboard.py
+++ app/accounts/presentation/views/dashboard.py
@@ -1,7 +1,23 @@
+from django.db import models
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from common.utils.common_queries import get_application
+from application.models import ApplicationDocument
+
 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
 
     template_name = "profile/dashboard.html"
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        application, msg = get_application(self.request.user)
+        context['application'] = application
+        return context
+
+
+class ExtraDocumentView(CreateView):
+    model = ApplicationDocument
