1eee24289870bd2961bed7f0273626952f560ad0
xcixor <pndungu54@gmail.com>
2022-03-13 13:54:27 +0300

    fix(permissions): add correct permissions to view eligibility page

--- app/eligibility/presentation/views/eligibility.py
+++ app/eligibility/presentation/views/eligibility.py
@@ -1,8 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
+
+User = get_user_model()
 
 
 class EligibilityView(PermissionRequiredMixin, TemplateView):
 
     template_name = 'eligibility/eligibility.html'
-    permission_required = ('application.view_application', )
+    permission_required = ('application.can_view_application', )
+    permission_denied_message = (
+        'Hmm it seems that you cannot view this application '
+        'please contact your admin.')
