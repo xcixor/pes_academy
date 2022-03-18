ed2e1350e2fddcfba1cf7d4f85aa76f1da101f7c
xcixor <pndungu54@gmail.com>
2022-03-18 12:25:25 +0300

    feat(admin): display comments and scores per question in admin dashboard

--- app/application/models/application_score.py
+++ app/application/models/application_score.py
@@ -19,7 +19,8 @@ class ApplicationScore(models.Model):
     prompt = models.TextField()
     application = models.ForeignKey(
         Application, on_delete=models.CASCADE, related_name='scores')
-    reviewer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='scores')
+    reviewer = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name='scores')
 
     def __str__(self):
         return f'{self.get_score_display()} score for {self.prompt} for {self.application.special_id}'
--- app/application/templatetags/application_extras.py
+++ app/application/templatetags/application_extras.py
@@ -16,3 +16,13 @@ def get_reviewer_scores_for_application(reviewer, application):
 @register.filter('get_prompts')
 def get_prompts(reviewer, application):
     return reviewer.prompts.filter(application=application)
+
+
+@register.filter('get_comments')
+def get_comments(reviewer, application):
+    return reviewer.comments.filter(application=application)
+
+
+@register.filter('get_question_scores_for_application')
+def get_question_scores_for_application(reviewer, application):
+    return reviewer.scores.filter(application=application)
