641202bab65c94863fd26d3c58388265e8266656
xcixor <pndungu54@gmail.com>
2022-04-01 10:06:19 +0300

    chore(media): add cloud settings to save db images

--- app/core/settings/production.py
+++ app/core/settings/production.py
@@ -1,9 +1,18 @@
 from .base import *
+from google.oauth2 import service_account
+
+
 DEBUG = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-# media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# media files config
+MEDIA_URL = os.environ.get('GS_BUCKET_URL')
+DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+GS_BUCKET_NAME = os.environ.get('GS_BUCKET_NAME')
+GS_LOCATION = os.environ.get('GS_LOCATION')
+
+GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
+    os.environ.get('GS_CREDENTIALS')
+)
