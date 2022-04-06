97aee6a3f47770450b0d126ff5abd0dd63c31401
xcixor <pndungu54@gmail.com>
2022-04-06 20:51:25 +0300

    fix(deployment): change redis ports to fix tests

--- scripts/entrypoint.prod.sh
+++ scripts/entrypoint.prod.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 redis-server /etc/redis/redis.conf protected-mode no
 # test redis
-redis-cli -h localhost -p 6380 ping
+redis-cli -h localhost -p 6379 ping
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
