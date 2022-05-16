fec7e3bb443de6ead0f6743fba09809b382d4164
xcixor <pndungu54@gmail.com>
2022-05-16 12:52:34 +0300

    chore(admin): register staff models

--- app/staff/admin.py
+++ app/staff/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from staff.models import Session
+from staff.models import Session, SessionMaterial, Meeting
 
 admin.site.register(Session)
+admin.site.register(SessionMaterial)
+admin.site.register(Meeting)
