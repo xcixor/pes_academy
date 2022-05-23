70f99eaf5b8d13741ea77ddae5756d38ffd81647
xcixor <pndungu54@gmail.com>
2022-05-23 13:05:00 +0300

    fix(test): fix failing test

--- app/accounts/tests/presentation/test_help_view.py
+++ app/accounts/tests/presentation/test_help_view.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView
+from django.views.generic import FormView
 from accounts.presentation.views import HelpView
 from django.test import TestCase
 
@@ -10,7 +10,7 @@ class HelpViewTestCase(TestCase):
 
     def test_view_properties(self):
         self.assertEqual(HelpView.template_name, 'help.html')
-        self.assertTrue(issubclass(HelpView, TemplateView))
+        self.assertTrue(issubclass(HelpView, FormView))
 
     def test_successfully_gets_help_page(self):
         response = self.client.get('/accounts/help/')
