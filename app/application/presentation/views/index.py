3d13aee66e78bb786b2c281e481e5ded3049afa3
xcixor <pndungu54@gmail.com>
2022-07-21 19:37:04 +0300

    chore(applications): modify index route to redirect to agripitch application

--- app/application/presentation/views/index.py
+++ app/application/presentation/views/index.py
@@ -1,12 +1,12 @@
 from django.views.generic import ListView
 from django.utils import timezone
-from application.models import CallToAction
+from agripitch.models import Competition
 
 
 class IndexView(ListView):
 
     template_name = "index/index.html"
-    model = CallToAction
+    model = Competition
     context_object_name = 'calls_to_action'
 
     def get_queryset(self):
