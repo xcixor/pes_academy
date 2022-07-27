fd1bb56b46463f3442064cd7d1764449d42664c2
xcixor <pndungu54@gmail.com>
2022-07-27 16:04:58 +0300

    refactor(app): move form related code to a form module

--- app/agripitch/admin.py
+++ app/agripitch/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django import forms
+from agripitch.forms import CustomSubCriteriaItemAdminForm
+
 from agripitch.models import (
     ShortList, CriteriaItem,
     SubCriteriaItem, SubCriteriaItemChoice, SubCriteriaItemResponse,
@@ -18,23 +19,6 @@ class CriteriaItemAdmin(admin.ModelAdmin):
 @admin.register(SubCriteriaItemFieldProperties)
 class SubCriteriaItemFieldPropertiesAdmin(admin.ModelAdmin):
     list_display = ['sub_criteria_item', 'name', 'value']
-
-
-class CustomSubCriteriaItemAdminForm(forms.ModelForm):
-    class Meta:
-        model = SubCriteriaItem
-        fields = '__all__'
-
-    def __init__(self, *args, **kwargs):
-        super(CustomSubCriteriaItemAdminForm, self).__init__(*args, **kwargs)
-        choices = [(index+1, index+1) for index, item in enumerate(SubCriteriaItem.objects.filter(
-            criteria=self.instance.criteria))]
-        self.fields['position_in_form'] = forms.ChoiceField()
-        self.fields['position_in_form'].choices = choices
-        self.fields['label'].initial = self.instance.label
-        self.fields['criteria'].initial = self.instance.criteria
-        self.fields['type'].initial = self.instance.type
-        self.fields['position_in_form'].initial = self.instance.position_in_form
 
 
 @admin.register(SubCriteriaItem)
---
+++ app/agripitch/forms/model_forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from agripitch.models import SubCriteriaItem
+
+
+class CustomSubCriteriaItemAdminForm(forms.ModelForm):
+    class Meta:
+        model = SubCriteriaItem
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(CustomSubCriteriaItemAdminForm, self).__init__(*args, **kwargs)
+        choices = [
+            (index+1, index+1) for index, item in enumerate(
+                SubCriteriaItem.objects.filter(criteria=self.instance.criteria))]
+        self.fields['position_in_form'] = forms.ChoiceField()
+        self.fields['position_in_form'].choices = choices
+        self.fields['label'].initial = self.instance.label
+        self.fields['criteria'].initial = self.instance.criteria
+        self.fields['type'].initial = self.instance.type
+        self.fields['position_in_form'].initial = self.instance.position_in_form
