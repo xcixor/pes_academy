30bc767f1723623d47d14f9cbc19ebdef245abfa
xcixor <pndungu54@gmail.com>
2022-08-22 11:28:16 +0300

    chore(application): add application to context directly

--- app/agripitch/presentation/views/landing.py
+++ app/agripitch/presentation/views/landing.py
@@ -1,28 +1,18 @@
-from django.views.generic import ListView
-from django.utils import timezone
+from django.views.generic import TemplateView
 from application.models import CallToAction, CarouselImage
 
 
-class AgripitchLandingView(ListView):
+class AgripitchLandingView(TemplateView):
 
     template_name = "agripitch/landing.html"
-    model = CallToAction
     context_object_name = 'calls_to_action'
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        queryset = queryset.filter(
-            available_for_applications=True,
-            deadline__gte=timezone.now())
-        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        queryset = super().get_queryset()
         if CarouselImage.objects.all():
             first_image = CarouselImage.objects.first()
             context['first_carousel_image'] = first_image
             context['other_carousel_images'] = CarouselImage.objects.exclude(
                 id=first_image.id)
-        context['application'] = queryset.first()
+        context['application'] = CallToAction.objects.first()
         return context
