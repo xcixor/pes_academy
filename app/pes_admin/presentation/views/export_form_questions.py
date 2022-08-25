018cd8e1354f8a85603d27161e2c410105558421
xcixor <pndungu54@gmail.com>
2022-08-25 14:49:13 +0300

    feat(questions xls): add useful properties to excel

--- app/pes_admin/presentation/views/export_form_questions.py
+++ app/pes_admin/presentation/views/export_form_questions.py
@@ -12,25 +12,38 @@ def export_agripitch_questions_xls(request):
     ws = wb.add_sheet('Questions')
 
     # Sheet header, first row
-    row_num = 0
-
     font_style = xlwt.XFStyle()
     font_style.font.bold = True
+    font_style.alignment.wrap = 1
+    header = (
+        'These are the updated questions \n'
+        'Questions ending with * means they are required'
+    )
+    ws.write_merge(0, 0, 0, 5, header, font_style)
 
-    columns = ['Question', 'Criteria', ]
-
+    # question title headings
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+    columns = ['Question', 'Answer Type', 'Choices']
+    row_num = 1
     for col_num in range(len(columns)):
         ws.write(row_num, col_num, columns[col_num], font_style)
-
     # Sheet body, remaining rows
-    font_style = xlwt.XFStyle()
+    style = xlwt.XFStyle()
+    style.alignment.wrap = 1
 
-    rows = SubCriteriaItem.objects.all().values_list(
-        'label', 'criteria__label')
+    rows = SubCriteriaItem.objects.all().values_list('label')
+    updated_rows = []
     for row in rows:
+        object = SubCriteriaItem.objects.get(label=row[0])
+        type = [object.get_type_display()]
+        choices = object.choices.all()
+        choice_list = [choice.choice + "\n" for choice in choices]
+        updated_row = row + tuple(type) + tuple([choice_list])
+        updated_rows.append(updated_row)
+    for row in updated_rows:
         row_num += 1
         for col_num in range(len(row)):
-            ws.write(row_num, col_num, row[col_num], font_style)
-
+            ws.write(row_num, col_num, row[col_num], style)
     wb.save(response)
     return response
