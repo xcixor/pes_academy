bca517a8fb5e8d7775f687b51fdad494228eceac
Bryanaim <isale.bryan@gmail.com>
2022-08-30 09:59:08 +0300

    ui-fixes

--- app/application/static/js/script.js
+++ app/application/static/js/script.js
@@ -28,6 +28,46 @@ gsap.fromTo('.hero-textbox',{yPercent: 40,opacity: 0},{duration: 1,delay: 1.3,yP
        $this.addClass("show-desc");
     }
   });
+
+  const countdown = document.querySelector('.countdown');
+
+// Set Launch Date (ms)
+const launchDate = new Date('Sep 26, 2022 12:00:00').getTime();
+
+// Update every second
+const intvl = setInterval(() => {
+  // Get todays date and time (ms)
+  const now = new Date().getTime();
+
+  // Distance from now and the launch date (ms)
+  const distance = launchDate - now;
+
+  // Time calculation
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display result
+  countdown.innerHTML = `
+  <div class="bg-secondary">${days}<span>Days</span></div> 
+  <div>${hours}<span>Hours</span></div>
+  <div>${mins}<span>Minutes</span></div>
+  <div>${seconds}<span>Seconds</span></div>
+  `;
+
+  // If launch date is reached
+  if (distance < 0) {
+    // Stop countdown
+    clearInterval(intvl);
+    // Style and output text
+    countdown.style.color = '#17a2b8';
+    countdown.innerHTML = 'Launched!';
+  }
+}, 1000);
+
 });
 
 
