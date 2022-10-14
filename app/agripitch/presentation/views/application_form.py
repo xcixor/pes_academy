8a1f40c917dd2d49abf7d9cf6494729d15765751
xcixor <pndungu54@gmail.com>
2022-10-14 12:50:53 +0300

    patch(locale): add missing french translations

--- app/agripitch/presentation/views/application_form.py
+++ app/agripitch/presentation/views/application_form.py
@@ -48,7 +48,7 @@ class GetApplicationFormView(DetailView):
         self.object = self.get_object()
         if not self.object.available_for_applications or self.object.deadline < timezone.now():
             message = _(
-                "The competition is not yet available!")
+                "Sorry, the AFDB Agripitch Competition 2022 is not yet live!")
             messages.add_message(
                 request, messages.INFO, message)
             return redirect(reverse('agripitch:agripitch_landing_page'))
