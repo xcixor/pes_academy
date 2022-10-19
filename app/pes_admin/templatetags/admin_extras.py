00aeb6b7cb3adadf574f477b9b3c06ae0596f659
xcixor <pndungu54@gmail.com>
2022-10-19 04:30:40 +0300

    feat(admin): add chart to show applications by stage

--- app/pes_admin/templatetags/admin_extras.py
+++ app/pes_admin/templatetags/admin_extras.py
@@ -2,6 +2,7 @@ from django import template
 from django_countries import countries
 from application.models import Application
 from agripitch.models import SubCriteriaItem, SubCriteriaItemResponse
+from accounts.models import User
 
 register = template.Library()
 
@@ -27,6 +28,33 @@ def get_total_applications_by_criteria(criteria):
             title = dict(countries)[title]
         applications_by_criteria.append(
             [title, SubCriteriaItemResponse.objects.filter(
-                    value=application.value).count()]
+                value=application.value).count()]
         )
     return applications_by_criteria
+
+
+@register.filter('get_applications_by_stage')
+def get_applications_by_stage(string_to_invoke_call):
+    applications_by_stage = []
+    step_one_applications = Application.objects.filter(
+        stage='step_one').count()
+    step_two_applications = Application.objects.filter(
+        stage='step_two').count()
+    step_three_applications = Application.objects.filter(
+        stage='step_three').count()
+    step_four_applications = Application.objects.filter(
+        stage='step_four').count()
+    applications_by_stage = [
+        ['Application Data Not Submitted', step_one_applications],
+        ['Data Submitted', step_two_applications],
+        ['Documents in review', step_three_applications],
+        ['Verdict Passed', step_four_applications],
+        ]
+    print(applications_by_stage)
+    return applications_by_stage
+
+
+@register.filter('get_total_afdb_applicants')
+def get_total_afdb_applicants(string_to_invoke_call):
+    applicants = User.objects.filter(is_applying_for_a_call_to_action=True)
+    return applicants
