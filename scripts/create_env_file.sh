86c7899bc027bda999ebd4e37f4af7c055135f3a
xcixor <pndungu54@gmail.com>
2022-10-21 08:36:33 +0300

    fix(container): add ADMIN_EMAILS to container environment

--- scripts/create_env_file.sh
+++ scripts/create_env_file.sh
@@ -4,6 +4,7 @@ create_env_file() {
     touch .env
     echo "EMAIL_HOST_PASSWORD=${EMAIL_HOST_PASSWORD}" >> .env
     echo "VERIFIED_EMAIL_USER=${VERIFIED_EMAIL_USER}" >> .env
+    echo "ADMIN_EMAILS=${ADMIN_EMAILS}" >> .env
 }
 main(){
     create_env_file
