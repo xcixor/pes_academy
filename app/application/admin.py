72503407a1270907ee975116c61de66ead4287ce
xcixor <pndungu54@gmail.com>
2022-10-31 05:04:34 +0300

    chore(admin): remove ApplicationAdmin inlines which seem to be causing an issue

--- app/application/admin.py
+++ app/application/admin.py
@@ -59,11 +59,6 @@ class ApplicationAdmin(admin.ModelAdmin):
     list_display = ['special_id', 'application_creator', 'created']
     list_filter = ['stage']
 
-    inlines = [
-        ApplicationDocumentInline, ApplicationReviewInline,
-        ApplicationScoreInline, ApplicationPromptInline,
-        ApplicationCommentInline]
-
 
 @admin.register(ApplicationScore)
 class ApplicationScoreAdmin(admin.ModelAdmin):
