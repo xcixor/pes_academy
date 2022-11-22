1d1c39045216dd138adae74da8c51cf9022c24d9
xcixor <pndungu54@gmail.com>
2022-11-22 13:52:57 +0300

    fix(eligibility): fix question comment not saving error

--- app/application/presentation/views/question_comment.py
+++ app/application/presentation/views/question_comment.py
@@ -1,4 +1,8 @@
 from django.views.generic import CreateView
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.utils.translation import gettext_lazy as _
+from django import forms
 from application.models import QuestionComment
 
 
@@ -9,5 +13,16 @@ class QuestionCommentView(CreateView):
     fields = '__all__'
 
     def get_success_url(self) -> str:
+        message = _('Great your comment has been saved!.')
+        messages.add_message(self.request, messages.SUCCESS, message)
         slug = self.kwargs.get('slug')
         return f'/eligibility/{slug}/'
+
+    def form_invalid(self, form):
+        message = _(
+            'Something went wrong, please check your form below.')
+        messages.add_message(self.request, messages.ERROR, message)
+        context = self.get_context_data()
+        context['form'] = form
+        application_slug = self.kwargs.get('slug')
+        return redirect(f'/eligibility/{application_slug}/')
