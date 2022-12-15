c6849c22939f63499a34054b316aa75c56f1d90c
xcixor <pndungu54@gmail.com>
2022-12-15 12:36:46 +0300

    chore(scripts): update scripts

--- app/scripts/fixing.py
+++ app/scripts/fixing.py
@@ -54,6 +54,70 @@ def restore_applications():
             pk = item['pk']
         print(f'Application with pk: {pk} already exists')
 
+def get_model_data(model):
+    questions = []
+    with open('db_backups/nov_29_17_41.json') as data:
+        parsed_data = json.load(data)
+        for item in parsed_data:
+            if item['model'] == model:
+                questions.append(item)
+    return questions
+
+
+def restore_application_marks(owner_id):
+    data = get_model_data("agripitch.applicationmarks")
+    for item in data:
+        field_data = item['fields']
+        application_object = Application.objects.get(
+            pk=field_data['application'])
+        if str(application_object.application_creator) == owner_id:
+            sub_criteria_item = SubCriteriaItem.objects.get(
+                pk=field_data['question'])
+            scoring = Scoring.objects.get(
+                pk=field_data['scoring'])
+            pk = item['pk']
+            field_data.pop('application')
+            field_data.pop('question')
+            field_data.pop('scoring')
+            try:
+                print(ApplicationMarks.objects.update_or_create(
+                    pk=pk, question=sub_criteria_item,
+                    application=application_object,
+                    scoring=scoring, **field_data))
+            except:
+                print("Marks already exist")
+
+def restore_bonus(owner_id):
+    data = get_model_data("eligibility.bonuspoints")
+    for item in data:
+        field_data = item['fields']
+        application_object = Application.objects.get(
+            pk=field_data['application'])
+        if str(application_object.application_creator) == owner_id:
+            pk = item['pk']
+            step = ShortListGroup.objects.get(pk=field_data['step'])
+            field_data.pop('application')
+            field_data.pop('step')
+            try:
+                print(BonusPoints.objects.update_or_create(
+                    pk=pk, application=application_object, step=step, **field_data))
+            except:
+                print("Bonus already exists")
+
+def get_total_marks(owner_id):
+    owner = User.objects.get(id=owner_id)
+    app = Application.objects.get(application_creator=owner)
+    return app.overall_score
 
 def run():
-    restore_applications()
+    with open('esthers.csv') as data_file:
+        reader = csv.reader(data_file)
+        # for row in reader:
+        #     owner_id = row[0]
+        #     restore_bonus(owner_id)
+        with open('esthers_marks_2.csv', 'w', newline='') as out_file:
+            writer = csv.writer(out_file)
+            for row in reader:
+                owner_id = row[0]
+                writer.writerow([owner_id, get_total_marks(owner_id)])
+    print('done')
