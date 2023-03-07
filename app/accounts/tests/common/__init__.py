0172dd216db7971f37059abfc281b213a068d044
xcixor <pndungu54@gmail.com>
2023-03-07 09:36:32 +0300

    refactor(tests): restore testing

--- app/accounts/tests/common/__init__.py
+++ app/accounts/tests/common/__init__.py
@@ -1,13 +1,14 @@
 from django.contrib.auth import get_user_model
-from application.tests.common import ApplicationBaseTestCase
-from application.models import BusinessOrganization, Milestone, CovidImpact
-from application.models import Application
+# from application.tests.common import ApplicationBaseTestCase
+# from application.models import BusinessOrganization, Milestone, CovidImpact
+# from application.models import Application
+from common.tests.base_test_case import BaseTestCase
 
 
 User = get_user_model()
 
 
-class AccountsBaseTestCase(ApplicationBaseTestCase):
+class AccountsBaseTestCase(BaseTestCase):
 
     def setUp(self):
         super(AccountsBaseTestCase, self).setUp()
@@ -20,44 +21,3 @@ class AccountsBaseTestCase(ApplicationBaseTestCase):
         user.save()
         return user
 
-    def create_application(self):
-        call_to_action = self.create_call_to_action_instance()
-        application = Application.objects.create(
-            application_creator=self.user,
-            call_to_action=call_to_action,
-            slug=call_to_action.slug
-        )
-        return application
-
-    def create_business(self):
-        link = BusinessOrganization.objects.create(
-            organization_owner=self.user,
-            organization_name='Caravan Tech',
-            facebook_link='https://faceme.com',
-            twitter_link='https://twitterme.com',
-            whatsapp_business_link='https://whatsapp.com',
-            instagram_link='https://instagram.com',
-            value_chain='chain_one',
-            existence_period='period_one',
-            stage='stage_one',
-        )
-        return link
-
-    def create_milestone(self):
-        milestone = Milestone.objects.create(
-            milestone='Revenue Growth'
-        )
-        return milestone
-
-    def create_another_milestone(self):
-        milestone = Milestone.objects.create(
-            milestone='Market Segmentation'
-        )
-        return milestone
-
-    def create_covid_impact(self):
-        impact = CovidImpact.objects.create(
-            business=self.create_business(),
-            impact='Loss of Customers',
-        )
-        return impact
